39cb2243aeecaeb4587472507cfbe3bdba11053e
Lachee <lachlan.h@hotmail.com>
2021-04-26 18:45:05 +1000

    Updated to modern manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+version '1.0.0'
+author 'Lachee'
+description 'Modified version of a garage script.'
+repository 'https://github.com/lachee/fivem-garage'
+
+client_scripts {
+	"config.lua",
+	"client/garageMenu.lua",
+	"client/gui.lua",
+	"client/functions.lua",
+	"client/main.lua"
+}
+
+server_scripts {
+	"@mysql-async/lib/MySQL.lua",
+	"config.lua",
+	"server/main.lua",
+}
+
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+
+dependencies {
+    'mysql-async'
+}
